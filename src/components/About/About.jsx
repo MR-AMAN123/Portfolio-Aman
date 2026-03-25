ddab2279157989c5ed9ddf70e8f9917a3293f242
Mohd_Aman <mohd2310021@akgec.ac.in>
2026-03-25 22:34:46 +0530

    updates google drive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,20 +10,19 @@ const About = () => {
       className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32 transition-colors duration-500"
     >
       <div className="flex flex-col-reverse md:flex-row justify-between items-center">
-        
-        {/* Left Side */}
+      
         <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0 lg:-ml-42 md:-ml-24 sm:ml-0 transition-all duration-500">
-          {/* Greeting */}
+          
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-2 leading-tight">
             Hi, I am
           </h1>
 
-          {/* Name */}
+       
           <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold  dark:text-white mb-4 leading-tight">
             Mohd Aman
           </h2>
 
-          {/* Skills Typing Effect */}
+        
           <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-purple-600 dark:text-[#8245ec] leading-tight">
             <span className="text-gray-900 dark:text-white">I am a </span>
             <ReactTypingEffect
@@ -38,7 +37,7 @@ const About = () => {
             />
           </h3>
 
-          {/* About Text */}
+         
           <p className="text-base sm:text-lg md:text-lg text-gray-700 dark:text-gray-400 mb-10 mt-8 leading-relaxed">
             I am a full-stack developer in
             building scalable web applications. Skilled in both front-end and
@@ -47,9 +46,9 @@ const About = () => {
             efficient solutions.
           </p>
 
-          {/* Resume Button */}
+         
           <a
-            href="https://drive.google.com/file/d/1BJkL7bXRNT8ESX1780e0V0xqWpRMn_lA/view"
+            href="https://drive.google.com/file/d/1reC9OysU-zg914NX2a_SrFGb_rLinIr2/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -62,7 +61,7 @@ const About = () => {
           </a>
         </div>
 
-        {/* Right Side */}
+       
         <div className="md:w-1/2 flex justify-center md:justify-end lg:-mr-28 md:mr-24 sm:mr-0 transition-all duration-500">
           <Tilt
             className="w-48 h-48 sm:w-64 sm:h-64 md:w-[28rem] md:h-[28rem] border-4 border-purple-600 rounded-full"
